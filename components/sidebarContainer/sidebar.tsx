2ffd260a6f34dcd54f040066b953587797b531fe
youness <adxus405@gmail.com>
2024-07-05 22:58:19 +0100

    styling 4

--- components/sidebarContainer/sidebar.tsx
+++ components/sidebarContainer/sidebar.tsx
@@ -3,9 +3,9 @@ import SidebarLinks from "./sidebarLinks";
 export default function Sidebar() {
     return (
         <nav className="bg-white drop-shadow-sm rounded-xl flex md:flex-col flex-row gap-4 md:py-8 md:px-4 p-2">
-            <SidebarLinks 
-            staticClassName="flex items-center gap-4 px-4 py-2" 
-            cls="bg-accent rounded-xl font-medium" />
+            <SidebarLinks
+                staticClassName="flex items-center gap-4 p-4"
+                cls="bg-accent rounded-xl font-medium" />
         </nav>
     )
 }
